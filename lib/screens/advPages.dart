c92977f4c080747c7968bd443831938a86a74e3c
belal_soliman <belalsoliman8@gmail.com>
2024-07-28 13:26:17 +0300

    make welcomes pahe tempelt

--- lib/screens/advPages.dart
+++ lib/screens/advPages.dart
@@ -6,7 +6,88 @@ class Advpages extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text("kos omk"),
+      body: Container(
+        width: double.infinity,
+        padding: EdgeInsets.all(16),
+        decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(20),
+            boxShadow: const [
+              BoxShadow(color: Colors.black12, blurRadius: 10, spreadRadius: 5),
+            ]),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(15),
+              child: Image.asset(
+                "assets/pexels-nida-kurt-5079840-12750409 (1).jpg",
+                height: 400,
+                width: double.infinity,
+                fit: BoxFit.fill,
+              ),
+            ),
+            const SizedBox(height: 16),
+            const Text(
+              '20% Discount',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const Text(
+              'New Arrival Product',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            const SizedBox(height: 8),
+            const Text(
+              'Publish up your selfies to make yourself more beautiful with this app.',
+              style: TextStyle(
+                fontSize: 16,
+                color: Colors.grey,
+              ),
+            ),
+            const SizedBox(height: 16),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.remove_circle_outline),
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.arrow_forward, color: Colors.black),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
+}
+
+class ImageClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    Path path = Path();
+    path.moveTo(0, 0);
+    path.lineTo(size.width, 0);
+    path.lineTo(size.width, size.height * 0.9);
+    path.quadraticBezierTo(
+      size.width * 0.5,
+      size.height,
+      0,
+      size.height * 0.9,
+    );
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) => false;
 }
